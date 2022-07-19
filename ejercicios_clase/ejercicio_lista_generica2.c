5d1260a3ea99100c3ea8e07698ecfbcf662574eb
Mozcko <musquiz02@gmail.com>
2022-07-18 21:14:14 -0500

    clase 18 julio
    falta terminar la tarea

---
+++ ejercicios_clase/ejercicio_lista_generica2.c
@@ -0,0 +1,256 @@
+#include<stdio.h>
+//#include<conio.h>
+#include<stdlib.h>
+
+char FIGURAS[4][20]= {"CORAZONES","TREBOLES","ESPADAS","DIAMANTES"};
+
+struct carta
+{
+
+    char figura[20];
+    int num;
+};
+
+struct nodo
+{
+    struct carta info;
+    struct nodo *sig;
+};
+
+void insertar(int pos, struct carta x);
+void imprimir();
+struct carta extraer(int pos);
+void liberar();
+void aleatoria(int n);
+void menu();
+
+//variable global que apunta al primer nodo de la lista
+struct nodo *raiz=NULL;
+struct nodo *fondo=NULL;
+
+void insertar(int pos, struct carta x)
+{
+    if (pos <= cantidad() + 1)
+    {
+        struct nodo *nuevo;
+        nuevo=malloc(sizeof(struct nodo));
+        nuevo->info = x;
+        if (pos == 1)
+        {
+            nuevo->sig = raiz;
+            raiz = nuevo;
+        }
+        else
+            if (pos == cantidad() + 1)
+            {
+                struct nodo *reco = raiz;
+                while (reco->sig != NULL)
+                {
+                    reco = reco->sig;
+                }
+                reco->sig = nuevo;
+                nuevo->sig = NULL;
+            }
+            else
+            {
+                struct nodo *reco = raiz;
+                int f;
+                for (f = 1; f <= pos - 2; f++)
+                {
+                    reco = reco->sig;
+                }
+                struct nodo *siguiente = reco->sig;
+                reco->sig = nuevo;
+                nuevo->sig = siguiente;
+            }
+    }
+}
+
+
+int vacia()
+{
+    if (raiz == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void imprimir()
+{
+    struct nodo *reco=raiz;
+    printf("Lista completa.\n");
+    while (reco!=NULL)
+    {
+        printf("%i de %s \n",reco->info.num, reco->info.figura);
+        reco=reco->sig;
+    }
+    printf("\n");
+}
+
+struct carta extraer(int pos)
+{
+    if (pos <= cantidad())
+    {
+        struct carta informacion;
+        struct nodo *bor;
+        if (pos == 1)
+        {
+            informacion = raiz->info;
+            bor = raiz;
+            raiz = raiz->sig;
+        }
+        else
+        {
+            struct nodo *reco;
+            reco = raiz;
+            int f;
+            for (f = 1; f <= pos - 2; f++)
+            {
+                reco = reco->sig;
+            }
+            struct nodo *prox = reco->sig;
+            reco->sig = prox->sig;
+            informacion = prox->info;
+            bor = prox;
+        }
+        free(bor);
+        return informacion;
+    }
+
+}
+
+void liberar()
+{
+    struct nodo *reco = raiz;
+    struct nodo *bor;
+    while (reco != NULL)
+    {
+        bor = reco;
+        reco = reco->sig;
+        free(bor);
+    }
+}
+
+
+void menu()
+{
+    int op,n, pos;
+    struct carta c1;
+    while(op != 6)
+    {
+
+        system("cls");
+        printf("Selecciona la opcion deseada\n"
+               "\t1\tInsertar Carta\n"
+               "\t2\tImprimir Cartas\n"
+               "\t3\tBorrar Carta\n"
+               "\t4\tCarta Aleatoria\n"
+               "\t5\tModificar Carta \n"
+               "\t6\tSalir \n"
+              );
+        scanf("%i",&op);
+        switch(op)
+        {
+        case 1:
+            printf("Escribe la posicion de la carta \n");
+            scanf("%i",&pos);
+            printf("Escribe la figura de la carta \n");
+            fflush(stdin);
+            gets(c1.figura);
+            printf("Escribe el numero de la carta \n");
+            scanf("%i",&c1.num);
+            insertar(pos,c1);
+            break;
+        case 2:
+            imprimir();
+            break;
+        case 3:
+            printf("Escribe la posicion de la carta \n");
+            scanf("%i",&pos);
+            c1=extraer(pos);
+            printf("se borro %i de %s \n", c1.num, c1.figura);
+
+            break;
+        case 4:
+            printf("Cuantas cartas deseas generar ?\n");
+            scanf("%i",&n);
+            aleatoria(n);
+            break;
+
+        case 5:
+            modificar();
+            break;
+        case 6:
+            liberar();
+            printf("Adios \n");
+            break;
+        default:
+            printf("Opcion Incorrecta \n");
+
+        }
+        system("pause");
+    }
+    return;
+}
+
+
+void aleatoria(int n)
+{
+    struct carta c;
+    for(int i=0; i<n; i++)
+    {
+        c.num = rand()%13 + 1;
+        strcpy(c.figura,FIGURAS[rand()%4]);
+        insertar(1,c);
+    }
+    return;
+}
+
+
+int cantidad()
+{
+    int n=0;
+    struct nodo *reco = raiz;
+    if(vacia()){
+        return n;
+    }
+    else{
+        while(reco != NULL)
+        {
+            n++;
+            reco= reco->sig;
+        }
+        return n;
+    }
+}
+
+void modificar()
+{
+    struct carta info;
+    int pos;
+    struct nodo *reco=raiz;
+    printf("Que posiciÃƒÂ³n quiere cambiar ?\n");
+    scanf("%i",&pos);
+    if(pos > cantidad() || pos < 1){
+        return;
+    }else{
+          printf("Cual es el nuevo valor de la figura?\n");
+          fflush(stdin);
+          gets(info.figura);
+          printf("Cual es el numero de la carta?\n");
+          scanf("%i",&info.num);
+          for(int i=1; i<pos; i++){
+            reco= reco->sig;
+          }
+          reco->info = info;
+    }
+    return;
+
+}
+
+int main()
+{
+    srand(time(NULL));
+    menu();
+    return 0;
+}
---
+++ semana10/ejercicio_listagenerica.c
@@ -0,0 +1,221 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct node
+{
+    int info;
+    struct node *next;
+};
+
+void insert(int pos, int x);
+void print();
+int extract(int pos);
+void modify();
+void freeList();
+void menu();
+int empty();
+int cantidad();
+
+struct node *first = NULL;
+struct node *last = NULL;
+
+void print()
+{
+    struct node *reco = first;
+    printf("Listado de todos los elementos de la cola.\n");
+    while (reco != NULL)
+    {
+        printf("%s - ", reco->info);
+        reco = reco->next;
+    }
+    printf("\n");
+}
+
+void insert(int pos, int x)
+{
+    if (pos <= cantidad() + 1)
+    {
+        struct node *new;
+        new = malloc(sizeof(struct node));
+        new->info = x;
+        if (pos == 1)
+        {
+            new->next = first;
+            first = new;
+        }
+        else
+        {
+            if (pos == cantidad() + 1)
+            {
+                struct node *reco = first;
+                while (reco->next != NULL)
+                {
+                    reco = reco->next;
+                }
+                reco->next = new;
+                new->next = NULL;
+            }
+            else
+            {
+                struct node *reco = first;
+                for (int i = 0; i <= pos - 2; i++)
+                {
+                    reco = reco->next;
+                }
+                struct node *next = reco->next;
+                reco->next = new;
+                new->next = next;
+            }
+        }
+    }
+}
+
+int extract(int pos)
+{
+    if (pos <= cantidad())
+    {
+        int informacion;
+        struct node *bor;
+        if (pos == 1)
+        {
+            informacion = first->info;
+            bor = first;
+            first = first->next;
+        }
+        else
+        {
+            struct node *reco;
+            reco = first;
+            for (int i = 1; i <= pos - 2; i++)
+            {
+                reco = reco->next;
+            }
+            struct node *prox = reco->next;
+            reco->next = prox->next;
+            informacion = prox->info;
+            bor = prox;
+        }
+        free(bor);
+        return informacion;
+    }
+    else
+        return -1;
+}
+
+void modify()
+{
+    int pos, x;
+    struct node *reco = first;
+    printf("que posición desea modificar? \n");
+    scanf("%i", &pos);
+    if (pos > cantidad() || pos < 1)
+    {
+        printf("posición no valida");
+        return;
+    }
+    else
+    {
+        printf("introduzca el nuevo valor para la posición %i", pos);
+        scanf("%i", x);
+        for (int i = 1; i < pos; i++)
+        {
+            reco = reco->next;
+        }
+        reco->info = x;
+    }
+    return;
+}
+
+void menu()
+{
+    int op, val, pos;
+    while (op != 5)
+    {
+
+        system("cls");
+        printf("Selecciona la opción deseada\n"
+               "\t1\tInsertar valor a la lista 1\n"
+               "\t2\tImprimir la lista\n"
+               "\t3\tExtraer nodo de la lista\n"
+               "\t4\tModificar un nodo de la lista\n"
+               "\t5\tSalir \n");
+        scanf("%i", &op);
+        switch (op)
+        {
+        case 1:
+            printf("escribe el valor a insertar \n");
+            scanf("%i", &val);
+            printf("escribe donde insertar el valor\n");
+            scanf("%i", &pos);
+            insert(pos, val);
+            break;
+        case 2:
+            print();
+            break;
+        case 3:
+            printf("escribe la posición del valor a extraer \n");
+            scanf("%i", &pos);
+            printf("Extraemos de la pila:%i\n", extract(pos));
+            break;
+        case 4:
+            modify();
+            break;
+        case 5:
+            freeList();
+            printf("Adios \n");
+            break;
+        default:
+            printf("Opción Incorrecta \n");
+        }
+        system("pause");
+    }
+    return;
+}
+
+int empty()
+{
+    if (first == NULL && last == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+int cantidad()
+{
+    int n = 0;
+    struct node *reco = first;
+    if (vacia())
+    {
+        return n;
+    }
+    else
+    {
+        while (reco != NULL)
+        {
+            n++;
+            reco = reco->next;
+        }
+        return n;
+    }
+}
+
+void freeList()
+{
+    struct node *reco = first;
+    struct node *bor;
+    while (reco != NULL)
+    {
+        bor = reco;
+        reco = reco->next;
+        free(bor);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL));
+    menu();
+    return 0;
+}
